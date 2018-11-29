b4d3af3efab3289edfd0840a8471bc74ecfb630c
BurakKaratay <45467000+BurakKaratay@users.noreply.github.com>
2018-11-29 23:55:26 +0300

    Add files via upload

---
+++ Algorithm.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+ifstream myfile;
+int x = 0;
+string line;
+myfile.open("questions.txt");
+while(myfile>>line){
+  if(x == 1 && line[line.length()-1] != '?'){
+    cout << line << endl;
+    x = 0;
+    cout << x << endl;
+}
+
+    if(line == "Who" || line == "who" || line == "Where" || line == "where" || line == "what" || line == "What" || line == "How" || line == "how"  ){
+    cout << line << endl;
+    x = x + 1;
+}
+    
+  
+if(line[line.   length()-1] == '?'){
+    line.erase(line.length()-1);
+}
+
+
+
+}
+return 0;
+}
